1816a62afaba33ab7e0f42327683e9118addb793
gerardo.itpro <gerardo.itpro@gmail.com>
2024-08-19 19:39:23 +0000

    First commit

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, HostBinding, ViewEncapsulation, input } from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculator-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './calculator-button.component.css',
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400',
+    // attribute: 'hello'
+    // 'data-size': 'XL',
+  },
+  // encapsulation: ViewEncapsulation.None,
+})
+export class CalculatorButtonComponent {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) => 
+      typeof value === 'string' ? value === '' : value,
+  });
+
+  public isDoubleSize = input(false, {
+    transform: (value: boolean | string) => 
+      typeof value === 'string' ? value === '' : value,
+  });
+
+    // @HostBinding('class.is-command') get commandStyle() {
+  //   return this.isCommand();
+  // }
+
+  @HostBinding('class.w-2/4') get commandStyle() {
+    return this.isDoubleSize();
+  }
+}
---
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CalculatorComponent } from "@/calculator/components/calculator/calculator.component";
+
+@Component({
+  selector: 'calculator-view',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CalculatorComponent
+],
+  templateUrl: './calculator-view.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CalculatorViewComponent { }
